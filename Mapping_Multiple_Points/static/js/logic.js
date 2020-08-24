703be55110c1e33b0ee9e69b7acffb01ff11cb15
viralp353 <65969608+viralp353@users.noreply.github.com>
2020-08-24 18:47:37 -0500

    Update logic.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -15,10 +15,45 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 streets.addTo(map)
 
 //  Add a marker to the map for Los Angeles, California.
-let marker1 = L.marker([34.0522, -118.2437]).addTo(map);
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circle([34.0522, -118.2437], {
-	color: 'black',
-	radius: 300,
-	maxZoom:18,
- }).addTo(map);
+//let marker1 = L.marker([34.0522, -118.2437]).addTo(map);
+////  Add a marker to the map for Los Angeles, California.
+//let marker = L.circle([34.0522, -118.2437], {
+//	color: 'black',
+//	radius: 300,
+//	maxZoom:18,
+// }).addTo(map);
+
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+ 
