a9492ad48bbb92ef226e5f8a8e5e46ff460eaf8b
viralp353 <65969608+viralp353@users.noreply.github.com>
2020-08-24 19:08:11 -0500

    update

--- Mapping_Lines/static/js/cities.js
+++ Mapping_Lines/static/js/cities.js
@@ -1,14 +1,13 @@
-// Get data from cities.js
-let cityData = cities;
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-	console.log(city)
-	L.circle(city.location,{
-        radius: city.population/10,
-        color: 'red',
-        fillColor: '#f03'
-        
-    })
-	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "red"
+}).addTo(map);
